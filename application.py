6cca29e576cba11f88246808dff528bd21902818
John Barczynski <john.barczynski@gmail.com>
2016-11-12 20:25:19 +0000

    modified to add api calls

--- application.py
+++ application.py
@@ -1,4 +1,7 @@
-from flask import Flask
+#Code based on presentation by Josh Li and Mayank Makwana at HackPSU 2016
+#https:github.com/
+
+from flask import Flask, request, jsonify
 
 application = Flask(__name__)
 
@@ -6,6 +9,14 @@ application = Flask(__name__)
 def index():
     return 'Hello World'
 
+@application.route('/api/getroute', methods=['GET'])
+def api_getroute():
+    args = request.args
+    output = {
+        'sum': int(args['x']) + int(args['y']),
+        'product': int(args['x']) * int(args['y'])
+    }
+    return jsonify(output)
+
 if __name__ == "__main__":
     application.run(debug=True)
-
